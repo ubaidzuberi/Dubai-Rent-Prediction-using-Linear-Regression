a971bc256cd8a710b2b84f60f893c346fcd78fd5
Ubaid Zuberi <113924862+ubaidzuberi@users.noreply.github.com>
2022-10-15 18:55:16 +0100

    Add files via upload

---
+++ server/util.py
@@ -0,0 +1,52 @@
+import pickle
+import json
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location,total_sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = bhk
+    x[1] = bath
+    x[2] = total_sqft
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    prediction = round(__model.predict([x])[0], -3)
+    if prediction<0:
+        return 0
+    else:
+        return prediction
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global  __data_columns
+    global __locations
+
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+
+    global __model
+    if __model is None:
+        with open('./artifacts/dubai_rent_model.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    print("loading saved artifacts...done")
+
+def get_location_names():
+    return __locations
+
+def get_data_columns():
+    return __data_columns
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
